8f6b622daec3be13d75331096dc2871ea7f6592b
Christopher <christopherclifford001@gmail.com>
2022-04-08 03:41:49 +0100

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Function begins
+ *
+ * Return: Function ends
+ */
+
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
