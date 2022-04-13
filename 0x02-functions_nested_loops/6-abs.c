46ec901d9b3cba0b0e5645ed8cb0be7f7179a001
Christopher <christopherclifford001@gmail.com>
2022-04-13 17:19:13 +0100

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - Prints the absolute values
+ * @n: parameter
+ * Return: The abosolute value
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
