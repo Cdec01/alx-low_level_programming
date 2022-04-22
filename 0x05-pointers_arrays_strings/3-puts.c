012e5fba4dade73a2e9b329585036af39e59923b
Christopher <christopherclifford001@gmail.com>
2022-04-22 04:04:02 +0100

    3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - prints the string
+ * @*str: pointer to the string
+ * Return: nothing
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
