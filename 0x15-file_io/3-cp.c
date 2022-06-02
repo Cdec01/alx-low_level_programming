57c5b1d9436549aa32cbbc60ca83e287e98d66f1
Christopher <christopherclifford001@gmail.com>
2022-06-02 20:22:54 +0100

    3.1

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,16 +1,76 @@
 #include "main.h"
 
 /**
+ * cp - copies file from a source to dest
+ * @file_from: file from
+ * @file_to: destination file
+ * Return: integer
+ */
+
+int cp(char *file_from, char *file_to)
+{
+	int pd, qd, qr, qw;
+	int qc, qpc;
+	char *buff[1024];
+
+	qc = open(file_from, O_RDONLY);
+	if (qc < 0)
+	{
+		return (98);
+	}
+
+	pd = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (pd < 0)
+	{
+		return (99);
+	}
+
+	qr = read(qd, buff, 1024);
+	if (qr < 0)
+	{
+		return (98);
+	}
+
+	while (qr > 0)
+	{
+		qw = write(pd, buff, qr);
+		if (qw < 0)
+		{
+			return (99);
+		}
+		qr = read(qd, buff, 1024);
+		if (qr < 0)
+		{
+			return (98);
+		}
+	}
+
+	qc = close(qd);
+	if (qc < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close qd %d\n", qc);
+		return (100);
+	}
+
+	qpc = close(qd);
+	if (qpc < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close qd %d\n", qpc);
+		return (100);
+	}
+	return (0);
+}
+
+/**
  * main - copies the content of a file to another file
  * @argc: number of arguments
  * @argv: array of arguments
  * Return: the actual number of letters read
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv)
 {
-	int file1, file2, n;
-	char buffer[1024];
+	int n;
 
 	if (argc != 3)
 	{
@@ -18,46 +78,20 @@ int main(int argc, char **argv)
 		exit(97);
 	}
 
-	file1 = open(argv[1], O_RDONLY);
-	if (file1 == -1)
+	n = cp(argv[1], argv[2]);
+	switch (n);
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+		case (98):
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
 
-	file2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (file2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-
-	while ((n = read(file1, buffer, 1024)) > 0)
-	{
-		if (write(file2, buffer, n) != n || file2 == -1)
-		{
+		case (99):
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
-		}
-	}
 
-	if (n == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		case (100):
+			exit(100);
+		default:
+			return (0);
 	}
-
-	if (close(file1) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(100);
-	}
-
-	if (close(file2) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
-		exit(100);
-	}
-
-	return (0);
 }
